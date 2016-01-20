d2a0200c56aa1a87cbafd2661eebb85454631be1
Evgeny V. Generalov <e.generalov@gmail.com>
2016-01-20 16:05:09 +0500

    improved po files finding

--- autotranslate/management/commands/translate_messages.py
+++ autotranslate/management/commands/translate_messages.py
@@ -1,3 +1,6 @@
+from __future__ import unicode_literals
+
+import glob
 import logging
 import os
 import re
@@ -6,6 +9,7 @@ from optparse import make_option
 import polib
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.utils._os import npath, upath
 
 from autotranslate.utils import translate_strings
 
@@ -20,10 +24,12 @@ class Command(BaseCommand):
         make_option('--locale', '-l', default=[], dest='locale', action='append',
                     help='autotranslate the message files for the given locale(s) (e.g. pt_BR). '
                          'can be used multiple times.'),
+        make_option('--exclude', '-x', dest='exclude', action='append', default=[],
+                    help='Locales to exclude. Default is none. Can be used multiple times.'),
         make_option('--untranslated', '-u', default=False, dest='skip_translated', action='store_true',
                     help='autotranslate the fuzzy and empty messages only.'),
-        make_option('--set-fuzzy', '-f', default=False, dest='set_fuzzy', action='store_true',
-                    help='set the fuzzy flag on autotranslated messages.'),
+        make_option('--set-fuzzy', '-f', dest='fuzzy', action='store_true', default=False,
+                    help='Set the fuzzy flag on autotranslated messages.'),
     )
 
     def add_arguments(self, parser):
@@ -34,51 +40,39 @@ class Command(BaseCommand):
         parser.add_argument('--locale', '-l', default=[], dest='locale', action='append',
                             help='autotranslate the message files for the given locale(s) (e.g. pt_BR). '
                                  'can be used multiple times.')
+        parser.add_argument('--exclude', '-x', dest='exclude', action='append', default=[],
+            help='Locales to exclude. Default is none. Can be used multiple times.')
         parser.add_argument('--untranslated', '-u', default=False, dest='skip_translated', action='store_true',
                             help='autotranslate the fuzzy and empty messages only.')
-        parser.add_argument('--set-fuzzy', '-f', default=False, dest='set_fuzzy', action='store_true',
-                            help='set the fuzzy flag on autotranslated messages.')
+        parser.add_argument('--set-fuzzy', '-f', dest='fuzzy', action='store_true', default=False,
+                            help='Set the fuzzy flag on autotranslated messages.')
 
     def set_options(self, **options):
         self.locale = options['locale']
+        self.exclude = options['exclude']
         self.skip_translated = options['skip_translated']
-        self.set_fuzzy = options['set_fuzzy']
+        self.set_fuzzy = options['fuzzy']
 
     def handle(self, *args, **options):
         self.set_options(**options)
 
-        assert getattr(settings, 'USE_I18N', False), 'i18n framework is disabled'
-        assert getattr(settings, 'LOCALE_PATHS', []), 'locale paths is not configured properly'
-        for directory in settings.LOCALE_PATHS:
-            # walk through all the paths
-            # and find all the pot files
-            for root, dirs, files in os.walk(directory):
-                for file in files:
-                    if not file.endswith('.po'):
-                        # process file only
-                        # if its a pot file
-                        continue
+        result = self.find_po_files(locale=self.locale, exclude=self.exclude)
+        for (dirname, filename) in result:
+            # get the target language from the parent folder name
+            target_language = os.path.basename(os.path.dirname(dirname))
+            fullname = os.path.join(dirname, filename)
+            self.translate_file(fullname, target_language)
 
-                    # get the target language from the parent folder name
-                    target_language = os.path.basename(os.path.dirname(root))
-
-                    if self.locale and target_language not in self.locale:
-                        logger.info('skipping translation for locale `{}`'.format(target_language))
-                        continue
-
-                    self.translate_file(root, file, target_language)
-
-    def translate_file(self, root, file_name, target_language):
+    def translate_file(self, filepath, target_language):
         """
         convenience method for translating a pot file
 
-        :param root:            the absolute path of folder where the file is present
-        :param file_name:       name of the file to be translated (it should be a pot file)
+        :param filepath:        path of the file to be translated (it should be a pot file)
         :param target_language: language in which the file needs to be translated
         """
         logger.info('filling up translations for locale `{}`'.format(target_language))
 
-        po = polib.pofile(os.path.join(root, file_name))
+        po = polib.pofile(filepath)
         strings = self.get_strings_to_translate(po)
         # translate the strings,
         # all the translated strings are returned
@@ -143,6 +137,54 @@ class Command(BaseCommand):
             # Set the 'fuzzy' flag on translation
             if self.set_fuzzy and 'fuzzy' not in entry.flags:
                 entry.flags.append('fuzzy')
+
+    def find_po_files(self, locale, exclude):
+        """Looks for `po` files.
+
+        This code was mostly copypasted from Django `compilemessages` management
+        command to be compatible with its search algorithm. See
+        https://github.com/django/django/blob/5d35b53/django/core/management/commands/compilemessages.py#L58-L95
+        """
+        basedirs = [os.path.join('conf', 'locale'), 'locale']
+        if os.environ.get('DJANGO_SETTINGS_MODULE'):
+            from django.conf import settings
+            basedirs.extend(upath(path) for path in settings.LOCALE_PATHS)
+
+        # Walk entire tree, looking for locale directories
+        for dirpath, dirnames, filenames in os.walk('.', topdown=True):
+            for dirname in dirnames:
+                if dirname == 'locale':
+                    basedirs.append(os.path.join(dirpath, dirname))
+
+        # Gather existing directories.
+        basedirs = set(map(os.path.abspath, filter(os.path.isdir, basedirs)))
+
+        if not basedirs:
+            raise CommandError("This script should be run from the Django Git "
+                               "checkout or your project or app tree, or with "
+                               "the settings module specified.")
+
+        # Build locale list
+        all_locales = []
+        for basedir in basedirs:
+            locale_dirs = filter(os.path.isdir, glob.glob('%s/*' % basedir))
+            all_locales.extend(map(os.path.basename, locale_dirs))
+
+        # Account for excluded locales
+        locales = locale or all_locales
+        locales = set(locales) - set(exclude)
+
+        for basedir in basedirs:
+            if locales:
+                dirs = [os.path.join(basedir, l, 'LC_MESSAGES') for l in locales]
+            else:
+                dirs = [basedir]
+            locations = []
+            for ldir in dirs:
+                for dirpath, dirnames, filenames in os.walk(ldir):
+                    for filename in filenames:
+                        if filename.endswith('.po'):
+                            yield (dirpath, filename)
 
 
 def humanize_placeholders(msgid):
